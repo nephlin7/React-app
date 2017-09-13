0221d5b06bca40fe528ac1db2c8a9ba43cd17991
Gerald Martinez <gmartinez@versatec.biz>
2017-09-13 16:19:23 -0600

    Add boostrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import GetEmployee  from './GetEmployee'
 import './App.css';
 
 class App extends Component {
@@ -9,12 +8,20 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React from heroku</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <GetEmployee />
+         <br />
+        <div className="container">
+          <div className="jumbotron">
+            <h1 className="display-3">Hello, world to React </h1>
+            <h2 className="display-4">from heroku!</h2>
+            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr className="my-4" />
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p className="lead">
+              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
