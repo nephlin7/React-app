846fa6bc16bceb887561dc65b4c67f172643e7ea
gmartinez <gmartinez@versatecenlinea.net>
2017-10-20 09:38:41 -0600

    change sample

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <div className="jumbotron">
             <h1 className="display-3">Hello, world to React </h1>
             <h2 className="display-4">from heroku!</h2>
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p className="lead">This is a simple hero unit proyect with react, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-4" />
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
             <p className="lead">
